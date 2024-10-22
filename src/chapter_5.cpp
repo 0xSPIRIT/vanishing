efc77319c483b09a65ae5f07819981bca1ef7b91
Ameer <ameergamez06@gmail.com>
2024-10-22 00:06:09 -0400

    Fixed the pillar uv maps in the first part of art gallery

--- src/chapter_5.cpp
+++ src/chapter_5.cpp
@@ -1966,7 +1966,7 @@ void chapter_5_init(Game *game) {
     level->models.real_head     = LoadModel(RES_DIR "models/real_head.glb");
     level->models.podium        = LoadModel(RES_DIR "models/podium.glb");
 
-    chapter_5_goto_scene(game, CHAPTER_5_SCENE_DESERT);
+    chapter_5_goto_scene(game, CHAPTER_5_SCENE_GALLERY);
 }
 
 void chapter_5_update_clerk(Game *game, float dt) {
